a42bfdd0de37a24a6a901ef66efa4a2111c2f984
VladislavChepusov <61112124+VladislavChepusov@users.noreply.github.com>
2023-01-20 20:39:23 +0300

    Загрузочный экран реализован

---
+++ app/src/main/java/com/example/projectbin/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.projectbin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+
+class SplashScreenActivity : AppCompatActivity() {
+    private val splashTime: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashTime)
+    }
+}
